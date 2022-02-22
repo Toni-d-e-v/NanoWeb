cb9e015e84160e7c52e50884b65523a323eccbdb
toni-d-e-v <toni.dumancic2@gmail.com>
2022-02-22 17:41:25 +0100

    small fixes

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module test
+module NanoWeb
 
 go 1.16
 
--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"github.com/julienschmidt/httprouter"
     "fmt"
@@ -7,41 +8,42 @@ import (
 	"os"
 	"io/ioutil"
 )
+
 func path(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	path := ps.ByName("path")
 	path_local := "./" + path
 	if path == "/" {
 		if _, err := os.Stat("./index.html"); err == nil {
 			http.ServeFile(w, r, "index.html")
-		} 
-		else {
+		} else {
 			w.Write([]byte("404"))
 		}
 	} else {
-		if _, err := os.Stat(path_local); err == nil {
-			if info, err := os.Stat(path_local); err == nil {
-				if info.IsDir() {
-					files, err := ioutil.ReadDir("." + path)
-					if err != nil {
-						log.Fatal(err)
-					}
-					for _, file := range files {
-						if info, err := os.Stat(path_local); err == nil {
-							if info.IsDir() {
-								fmt.Fprintf(w, "%s\n", file.Name())
-							}
+	if _, err := os.Stat(path_local); err == nil {
+		if info, err := os.Stat(path_local); err == nil {
+			if info.IsDir() {
+				files, err := ioutil.ReadDir("." + path)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, file := range files {
+					if info, err := os.Stat(path_local); err == nil {
+						if info.IsDir() {
+							fmt.Fprintf(w, "%s\n", file.Name())
 						}
 					}
-				} else {
-					http.ServeFile(w, r, "." + path)
 				}
-
+			} else {
+				http.ServeFile(w, r, "." + path)
 			}
-		} else {
-			fmt.Fprintf(w, "File not found ")
+
 		}
+	} else {
+		fmt.Fprintf(w, "File not found ")
+	}
 	}
 }
+
 func main() {
     router := httprouter.New()
 	port := os.Args[1]
